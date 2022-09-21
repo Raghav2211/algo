d7e2cb722692bed89ae851fcb97df462936fd9ee
Raghav2211 <rghvj.22@gmail.com>
2022-09-21 12:32:01 +0530

    binaryTree level order traversal

---
+++ leetcode_problems/src/com/github/devraghav/leetcode/BinaryTree_LevelOrderTraversal.java
@@ -0,0 +1,52 @@
+package com.github.devraghav.leetcode;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringJoiner;
+
+public class BinaryTree_LevelOrderTraversal {
+
+    // @formatter: off
+    /**
+     *       1
+     *    2      3
+     *  4  5   6   7
+     *
+     */
+    // @formatter: on
+
+    private StringJoiner traverse(TreeNode root, StringJoiner stringJoiner) {
+        if(root == null) return stringJoiner;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode treeNode = queue.poll();
+            stringJoiner.add(String.valueOf(treeNode.val));
+            if(treeNode.left !=null) {
+                queue.add(treeNode.left);
+            }
+            if(treeNode.right !=null) {
+                queue.add(treeNode.right);
+            }
+        }
+        return stringJoiner;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+
+        // create left tree
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // create right tree
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        BinaryTree_LevelOrderTraversal binaryTree_preOrderTraversal  = new BinaryTree_LevelOrderTraversal();
+        System.out.println(binaryTree_preOrderTraversal.traverse(root, new StringJoiner(",")));
+    }
+}
