f3ee724d346d98f0f0c7add899644495f8c07d3b
Raghav2211 <rghvj.22@gmail.com>
2022-09-22 19:03:29 +0530

    binaryTree is two tree identical

--- leetcode_problems/src/com/github/devraghav/leetcode/BinaryTree_IsSubTree.java
+++ leetcode_problems/src/com/github/devraghav/leetcode/BinaryTree_IsSubTree.java
@@ -3,9 +3,9 @@ package com.github.devraghav.leetcode;
 public class BinaryTree_IsSubTree {
 
     // @formatter: off
-    /**
-     *       1
-     *    2      3
+    /**    main                   subtree
+     *       1                       3
+     *    2      3                 6   7
      *  4  5   6   7
      *      8
      *
