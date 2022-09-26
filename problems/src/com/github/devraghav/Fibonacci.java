42d5453cd0b163f3ec11169e317b4a0e1add63d0
Raghav2211 <rghvj.22@gmail.com>
2022-09-26 17:56:15 +0530

    fibonacci with memoization

---
+++ problems/src/com/github/devraghav/Fibonacci.java
@@ -0,0 +1,30 @@
+package com.github.devraghav;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibonacci {
+
+    public long fibonacci(int n) {
+        return fibonacci(n, new HashMap<>());
+    }
+
+    public long fibonacci(int n, Map<Integer, Long> memoization) {
+        if (n <= 2) return 1;
+        if (memoization.containsKey(n)) return memoization.get(n);
+        memoization.put(n, (fibonacci(n - 1, memoization)
+                + fibonacci(n - 2, memoization)));
+        return memoization.get(n);
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci
+                = new Fibonacci();
+        System.out.println(fibonacci.fibonacci(5));
+        System.out.println(fibonacci.fibonacci(6));
+        System.out.println(fibonacci.fibonacci(7));
+        System.out.println(fibonacci.fibonacci(8));
+        System.out.println(fibonacci.fibonacci(50));
+        System.out.println(fibonacci.fibonacci(120));
+    }
+}
