9964d857f58c533c37e5a79f51b4770cb087a365
Raghav2211 <rghvj.22@gmail.com>
2022-10-03 22:57:40 +0530

    add memoization

--- problems/src/com/github/devraghav/dp/TargetSum.java
+++ problems/src/com/github/devraghav/dp/TargetSum.java
@@ -4,16 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TargetSum {
-    // TODO : memoization 
-    public int findTargetSumWays(int[] nums, int target, int len) {
+
+    private int findTargetSumWays(int[] nums, int target, int len, Map<String, Integer> memoization) {
+        var key = target + "-" + len;
+        if (memoization.get(key) != null) return memoization.get(key);
         if (target == 0 && len == 0) return 1;
         if (len == 0) return 0;
-        return findTargetSumWays(nums, target - nums[len - 1], len - 1) +
-                findTargetSumWays(nums, target + nums[len - 1], len - 1);
+        int ways = findTargetSumWays(nums, target - nums[len - 1], len - 1, memoization) +
+                findTargetSumWays(nums, target + nums[len - 1], len - 1, memoization);
+        memoization.put(key, ways);
+        return ways;
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        return findTargetSumWays(nums, target, nums.length);
+        return findTargetSumWays(nums, target, nums.length, new HashMap<>());
     }
 
     public static void main(String[] args) {
