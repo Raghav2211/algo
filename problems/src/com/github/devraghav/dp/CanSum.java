9e9133bc5deea081279d9158739ede4357f4febd
Raghav2211 <rghvj.22@gmail.com>
2022-10-03 17:39:14 +0530

    refactor

--- problems/src/com/github/devraghav/dp/CanSum.java
+++ problems/src/com/github/devraghav/dp/CanSum.java
@@ -6,13 +6,13 @@ import java.util.Map;
 // return true/false if targetSum can be achieved with the provided bucket
 public class CanSum {
 
-    private boolean bestSum(int[] bucket, int targetSum, int currentSum, Map<Integer, Boolean> memoization) {
+    private boolean exists(int[] bucket, int targetSum, int currentSum, Map<Integer, Boolean> memoization) {
         if (memoization.get(currentSum) != null) return memoization.get(currentSum);
         if (targetSum == currentSum) return true;
         if (targetSum < currentSum) return false;
         for (int i = 0; i < bucket.length; i++) {
             currentSum += bucket[i];
-            boolean isSumExists = bestSum(bucket, targetSum, currentSum, memoization);
+            boolean isSumExists = exists(bucket, targetSum, currentSum, memoization);
             if (isSumExists) {
 
                 return true;
@@ -23,17 +23,17 @@ public class CanSum {
         return false;
     }
 
-    public boolean bestSum(int[] bucket, int targetSum) {
-        return bestSum(bucket, targetSum, 0, new HashMap<>());
+    public boolean exists(int[] bucket, int targetSum) {
+        return exists(bucket, targetSum, 0, new HashMap<>());
     }
 
-    private boolean isSumExistsV2(int[] bucket, int targetSum, Map<Integer, Boolean> memoization) {
+    private boolean existsV2(int[] bucket, int targetSum, Map<Integer, Boolean> memoization) {
         if (memoization.get(targetSum) != null) return memoization.get(targetSum);
         if (targetSum == 0) return true;
         if (targetSum < 0) return false;
         for (int i = 0; i < bucket.length; i++) {
             int remain = targetSum - bucket[i];
-            if (isSumExistsV2(bucket, remain, memoization)) {
+            if (existsV2(bucket, remain, memoization)) {
                 return true;
             }
         }
@@ -41,27 +41,28 @@ public class CanSum {
         return false;
     }
 
-    public boolean isSumExistsV2(int[] bucket, int targetSum) {
-        return isSumExistsV2(bucket, targetSum, new HashMap<>());
+    public boolean existsV2(int[] bucket, int targetSum) {
+        return existsV2(bucket, targetSum, new HashMap<>());
     }
 
     public static void main(String[] args) {
         CanSum canSum = new CanSum();
-//        System.out.println(canSum.bestSum(new int[]{2, 3}, 7));
-//        System.out.println(canSum.bestSum(new int[]{5, 3, 4, 7}, 7));
-//        System.out.println(canSum.bestSum(new int[]{2, 4}, 7));
-//        System.out.println(canSum.bestSum(new int[]{2, 3, 5}, 8));
-//        System.out.println(canSum.bestSum(new int[]{7, 14}, 12000));
-//        System.out.println(canSum.bestSum(new int[]{3, 34, 4, 12, 5, 2}, 9));
-//        System.out.println(canSum.bestSum(new int[]{3, 34, 4, 12, 5, 2}, 30));
-        System.out.println(canSum.bestSum(new int[]{2, 4}, 10));
+        System.out.println(canSum.exists(new int[]{2, 3}, 7));
+        System.out.println(canSum.exists(new int[]{5, 3, 4, 7}, 7));
+        System.out.println(canSum.exists(new int[]{2, 4}, 7));
+        System.out.println(canSum.exists(new int[]{2, 3, 5}, 8));
+        System.out.println(canSum.exists(new int[]{7, 14}, 12000));
+        System.out.println(canSum.exists(new int[]{3, 34, 4, 12, 5, 2}, 9));
+        System.out.println(canSum.exists(new int[]{3, 34, 4, 12, 5, 2}, 30));
+        System.out.println(canSum.exists(new int[]{2, 4}, 10));
         System.out.println("--------------------V2--------------------");
-//        System.out.println(canSum.isSumExistsV2(new int[]{2, 3}, 7));
-//        System.out.println(canSum.isSumExistsV2(new int[]{5, 3, 4, 7}, 7));
-//        System.out.println(canSum.isSumExistsV2(new int[]{2, 4}, 7));
-//        System.out.println(canSum.isSumExistsV2(new int[]{2, 3, 5}, 8));
-//        System.out.println(canSum.isSumExistsV2(new int[]{7, 14}, 12000));
-//        System.out.println(canSum.isSumExistsV2(new int[]{3, 34, 4, 12, 5, 2}, 9));
-//        System.out.println(canSum.isSumExistsV2(new int[]{3, 34, 4, 12, 5, 2}, 30));
+        System.out.println(canSum.existsV2(new int[]{2, 3}, 7));
+        System.out.println(canSum.existsV2(new int[]{5, 3, 4, 7}, 7));
+        System.out.println(canSum.existsV2(new int[]{2, 4}, 7));
+        System.out.println(canSum.existsV2(new int[]{2, 3, 5}, 8));
+        System.out.println(canSum.existsV2(new int[]{7, 14}, 12000));
+        System.out.println(canSum.existsV2(new int[]{3, 34, 4, 12, 5, 2}, 9));
+        System.out.println(canSum.existsV2(new int[]{3, 34, 4, 12, 5, 2}, 30));
+        System.out.println(canSum.existsV2(new int[]{2, 4}, 10));
     }
 }
--- problems/src/com/github/devraghav/dp/HowSum.java
+++ problems/src/com/github/devraghav/dp/HowSum.java
@@ -7,13 +7,13 @@ import java.util.Map;
 
 public class HowSum {
 
-    public List<Integer> bestSum(int[] bucket, int targetSum, Map<Integer, List<Integer>> memoization) {
+    public List<Integer> doAndReturnList(int[] bucket, int targetSum, Map<Integer, List<Integer>> memoization) {
         if (memoization.containsKey(targetSum)) return memoization.get(targetSum);
         if (targetSum == 0) return new ArrayList<Integer>();
         if (targetSum < 0) return null;
         for (int j : bucket) {
             int remain = targetSum - j;
-            var elements = bestSum(bucket, remain);
+            var elements = doAndReturnList(bucket, remain);
             if (elements != null) {
                 elements.add(j);
                 memoization.put(targetSum, elements);
@@ -25,20 +25,20 @@ public class HowSum {
     }
 
 
-    public List<Integer> bestSum(int[] bucket, int targetSum) {
-        return bestSum(bucket, targetSum, new HashMap<>());
+    public List<Integer> doAndReturnList(int[] bucket, int targetSum) {
+        return doAndReturnList(bucket, targetSum, new HashMap<>());
     }
 
 
     public static void main(String[] args) {
         HowSum howSum = new HowSum();
-        System.out.println(howSum.bestSum(new int[]{2, 3}, 7));
-        System.out.println(howSum.bestSum(new int[]{5, 3, 4, 7}, 7));
-        System.out.println(howSum.bestSum(new int[]{2, 4}, 7));
-        System.out.println(howSum.bestSum(new int[]{2, 4}, 10));
-        System.out.println(howSum.bestSum(new int[]{2, 3, 5}, 8));
-        System.out.println(howSum.bestSum(new int[]{2, 4, 6, 7,  50, 32}, 3000));
-        System.out.println(howSum.bestSum(new int[]{3, 34, 4, 12, 5, 2}, 9));
-        System.out.println(howSum.bestSum(new int[]{3, 34, 4, 12, 5, 2}, 30));
+        System.out.println(howSum.doAndReturnList(new int[]{2, 3}, 7));
+        System.out.println(howSum.doAndReturnList(new int[]{5, 3, 4, 7}, 7));
+        System.out.println(howSum.doAndReturnList(new int[]{2, 4}, 7));
+        System.out.println(howSum.doAndReturnList(new int[]{2, 4}, 10));
+        System.out.println(howSum.doAndReturnList(new int[]{2, 3, 5}, 8));
+        System.out.println(howSum.doAndReturnList(new int[]{2, 4, 6, 7,  50, 32}, 3000));
+        System.out.println(howSum.doAndReturnList(new int[]{3, 34, 4, 12, 5, 2}, 9));
+        System.out.println(howSum.doAndReturnList(new int[]{3, 34, 4, 12, 5, 2}, 30));
     }
 }
